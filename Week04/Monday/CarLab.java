d58f6482f048b66440b5977420c1773a6755ab7c
Jin An <jin.an@dtcc.edu>
2026-02-02 14:47:29 -0500

    Create CarLab.java for car sales input
    Initialize car sales data and set January sales.

---
+++ Week04/Monday/CarLab.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CarLab {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int[] carSales = new int[12];
+        /*
+        carSales[0] <-- january
+        carSales[1]
+        carSales[2]
+        carSales[3]
+        ...
+        ..
+        carSales[11] <-- december
+         */
+
+        carSales[0] = 15;
+        carSales[1] = keyboard.nextInt();
+    }
+}
