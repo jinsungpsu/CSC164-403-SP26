3e47e758988118677f3e897d1e62d8f69f23ff9a
Jin An <jin.an@dtcc.edu>
2026-02-04 15:03:23 -0500

    Implement food ordering functionality
    Added functionality to store ordered food items and their details.

--- Week04/Wednesday/AssignmentMenuPrinting.java
+++ Week04/Wednesday/AssignmentMenuPrinting.java
@@ -1,3 +1,6 @@
+        /*
+        Assignment help!
+         */
         String[] foodNames = {"Burger", "Salad", "Pizza", "Soup", "Pasta"};
         double[] foodPrices = {5.99, 4.49, 8.99, 3.99, 7.49};
         String[] foodAllergens = {"Gluten, Dairy", "Nuts", "Gluten, Dairy", "None", "Gluten, Dairy"};
@@ -6,3 +9,23 @@
             System.out.printf("%nItem #%2d is %-8s costs $%5.2f, which has allergens: %s", (i+1), foodNames[i], foodPrices[i], foodAllergens[i]);
             //System.out.println("\nWhich is stored in index " + i + " of all 3 parallel arrays!");
         }
+
+        String[] orderedNames = new String[2];
+        double[] orderedPrices = new double[2];
+        String[] orderedAllergens = new String[2];
+
+        // if user entered item # 5 for their first item
+        orderedAllergens[0] = foodAllergens[4];
+        orderedPrices[0] = foodPrices[4];
+        orderedNames[0] = foodNames[4];
+
+        // second item, they chose pizza
+        orderedAllergens[1] = foodAllergens[2];
+        orderedPrices[1] = foodPrices[2];
+        orderedNames[1] = foodNames[2];
+
+        // alternatively
+        // store the ordered items in a single array
+        int[] orderedItems = new int[2];
+        orderedItems[0] = 4; // <-- pasta
+        orderedItems[1] = 2; // <-- pizza
