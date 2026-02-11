3ab949908f366bb1e1f72c4a5a2a0c571591bcb8
Jin An <jin.an@dtcc.edu>
2026-02-11 14:34:55 -0500

    Add FunctionsReview3 class with main method

---
+++ Week05/Wednesday/FunctionsReview3.java
@@ -0,0 +1,6 @@
+public class FunctionsReview3 {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},{2,3,4}};
+        double avg = FunctionsReview2.avgOfRow(arr,1);
+    }
+}
